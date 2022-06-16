530adaebc9d1e4e64b3961835475bb817376d18c
Arthur Akira <arthurak10.at@gmail.com>
2022-06-16 15:42:12 -0300

    chore: add /tweets get route

--- server/src/index.js
+++ server/src/index.js
@@ -18,19 +18,61 @@ const tweets = [
     username: "bob esponja calça quadrada",
     avatar:
       "https://img.elo7.com.br/product/zoom/3222EB9/bob-esponja-e-patrick-em-camadas-arquivo-de-corte-bob-esponja.jpg",
-    tweet: "Olá Patrick!",
+    tweet: "Tweet 1",
   },
   {
     username: "Naruto",
     avatar:
       "https://i.pinimg.com/originals/85/1b/ca/851bca4e7a0860ffaee2f647ee04b852.jpg",
-    tweet: "Meu melhor amigo é o Sasuke!!",
+    tweet: "Tweet 2",
   },
   {
     username: "sasuke uchiha",
     avatar:
       "https://i.pinimg.com/originals/8a/ab/a8/8aaba8fe5611ab66cca05f2d94c2932b.jpg",
-    tweet: "Não gosto muito do naruto...",
+    tweet: "Tweet 3",
+  },
+  {
+    username: "bob esponja calça quadrada",
+    avatar:
+      "https://img.elo7.com.br/product/zoom/3222EB9/bob-esponja-e-patrick-em-camadas-arquivo-de-corte-bob-esponja.jpg",
+    tweet: "Tweet 4",
+  },
+  {
+    username: "Naruto",
+    avatar:
+      "https://i.pinimg.com/originals/85/1b/ca/851bca4e7a0860ffaee2f647ee04b852.jpg",
+    tweet: "Tweet 5",
+  },
+  {
+    username: "sasuke uchiha",
+    avatar:
+      "https://i.pinimg.com/originals/8a/ab/a8/8aaba8fe5611ab66cca05f2d94c2932b.jpg",
+    tweet: "Tweet 6",
+  },
+  {
+    username: "bob esponja calça quadrada",
+    avatar:
+      "https://img.elo7.com.br/product/zoom/3222EB9/bob-esponja-e-patrick-em-camadas-arquivo-de-corte-bob-esponja.jpg",
+    tweet: "Tweet 7",
+  },
+  {
+    username: "Naruto",
+    avatar:
+      "https://i.pinimg.com/originals/85/1b/ca/851bca4e7a0860ffaee2f647ee04b852.jpg",
+    tweet: "Tweet 8",
+  },
+  {
+    username: "sasuke uchiha",
+    avatar:
+      "https://i.pinimg.com/originals/8a/ab/a8/8aaba8fe5611ab66cca05f2d94c2932b.jpg",
+    tweet: "Tweet 9",
+  },
+  {
+    username: "bob esponja calça quadrada",
+    avatar:
+      "https://img.elo7.com.br/product/zoom/3222EB9/bob-esponja-e-patrick-em-camadas-arquivo-de-corte-bob-esponja.jpg",
+    tweet: "Tweet 10",
   },
 ];
 
@@ -47,7 +89,9 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (_req, res) => {
-  res.send(tweets);
+  const lastTenTweets = tweets.slice(tweets.length - 10, tweets.length);
+
+  res.send(lastTenTweets.reverse());
 });
 
 app.listen(PORT, () => {
